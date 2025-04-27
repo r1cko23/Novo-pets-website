9a1026521ada31e9e7d83ee582cc3d9f26aed857
Jericko Razal <jericko.rzl@gmail.com>
2025-04-27 16:06:32 +0800

    hero image

--- client/src/components/home/Hero.tsx
+++ client/src/components/home/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
       <div 
         className="relative bg-cover bg-center h-[600px]" 
         style={{
-          backgroundImage: `url('https://images.unsplash.com/photo-1589135233689-dac091a7a923?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`
+          backgroundImage: `url('/images/novo_bg.jpeg')`
         }}
       >
         {/* Gradient Overlay */}
